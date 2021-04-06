9d6d20aea54afec3715fd92466d82619834eca8f
ianedmacd <81657598+ianedmacd@users.noreply.github.com>
2021-04-05 21:41:27 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -11,10 +11,8 @@
 	<title>Apex Tool Rentals</title>
 	
 	<!--Bootstrap/CSS/Font-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
